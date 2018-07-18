e47b95eca5445049f01e97a8635c6f79f13fca81
Michael Woolnough <michael.woolnough@gmail.com>
2018-07-18 14:41:43 +0100

    updated loader to use new rule interface

--- loader.js
+++ loader.js
@@ -55,30 +55,103 @@ function setCell(catA, rowA, catB, rowB, val, description) {
 }
 
 function addRule(catA, catB, rowB, method, catC, rowC, description) {
-	catA = catA.toUpperCase();
-	catB = catB.toUpperCase();
-	rowB = rowB.toUpperCase();
-	method = method.toUpperCase();
-	catC = catC.toUpperCase();
-	rowC = rowC.toUpperCase();
 	document.getElementsByTagName("button")[1].click();
-	var buttons = document.getElementById("choices").getElementsByTagName("button");
-	if (![
-		b => b.textContent.toUpperCase() == catA,
-		b => b.textContent.toUpperCase() == catB,
-		b => b.textContent.toUpperCase() == rowB,
-		b => b.textContent.toUpperCase() == method,
-		b => b.textContent.toUpperCase() == catC,
-		b => b.textContent.toUpperCase() == rowC
-	].every(fn => !Array.prototype.slice.apply(buttons).filter(fn).every(b => {b.click(); return false;}))) {
-		console.log("Invalid Rule: ", arguments);
-		document.getElementById("overlay").getElementsByTagName("button")[0].click();
+	var done = 0;
+	[
+		["categoryA", catA.toUpperCase()],
+		["categoryB", catB.toUpperCase()],
+		["valueB",    rowB.toUpperCase(), "categoryB"],
+		["categoryC", catC.toUpperCase()],
+		["valueC",    rowC.toUpperCase(), "categoryC"]
+	].every(function(v) {
+		var e = document.getElementById(v[0]),
+		    os = Array.from(e.childNodes),
+		    add = 0;
+		if (v.length === 3) {
+			os = Array.from(os[document.getElementById(v[2]).selectedIndex].childNodes);
+			add = (document.getElementById(v[2]).selectedIndex - 1) * os.length + 1;
+		}
+		return !os.every(function(c, n) {
+			if (c.hasAttribute("value") && c.getAttribute("value").toUpperCase() === v[1]) {
+				e.selectedIndex = add + n;
+				e.dispatchEvent(new CustomEvent("change"));
+				done++;
+				return false;
+			}
+			return true;
+		});
+	});
+	if (done < 5) {
+		document.getElementById("closer").click();
 		return false;
 	}
-	buttons[0].click();
-	if (typeof description !== "undefined") {
-		document.getElementsByTagName("table")[1].lastChild.getElementsByTagName("span")[0].setAttribute("title", description);
+
+	method = method.toUpperCase();
+
+	// Legacy
+	switch (method) {
+	case "LEFT/UP OF":
+		method = "EXACTLY 1 BEFORE";
+		break;
+	case "RIGHT/DOWN OF":
+		method = "EXACTLY 1 AFTER";
+		break;
+	case "ADJACENT TO":
+		method = "EXACTLY 1 ON ONE SIDE OF";
+		break;
 	}
+	var within = document.getElementById("within"),
+	    distance = document.getElementById("distance"),
+	    direction = document.getElementById("direction"),
+	    readNum = true;
+	if (method.startsWith("NOT ")) {
+		method = method.substring(4);
+		document.getElementById("not").checked = true;
+	}
+	if (method.startsWith("WITHIN ")) {
+		within.selectedIndex = 1;
+		within.dispatchEvent(new CustomEvent("change"));
+		method = method.substring(7);
+	} else if (method.startsWith("EXACTLY ")) {
+		within.selectedIndex = 2;
+		within.dispatchEvent(new CustomEvent("change"));
+		method = method.substring(8);
+	} else {
+		readNum = false;
+	}
+	if (readNum) {
+		var num = method.split(" ")[0];
+		if (Array.from(distance.childNodes).every(function(o, n) {
+			if (o.hasAttribute("value") && o.getAttribute("value") === num) {
+				distance.selectedIndex = n;
+				distance.dispatchEvent(new CustomEvent("change"));
+				method = method.substring(num.length + 1);
+				return false;
+			}
+			return true;
+		})) {
+			document.getElementById("closer").click();
+			return false;
+		}
+	}
+	switch (method) {
+	case "ON ONE SIDE OF":
+		direction.selectedIndex = 0;
+		direction.dispatchEvent(new CustomEvent("change"));
+		break;
+	case "AFTER":
+		direction.selectedIndex = 1;
+		direction.dispatchEvent(new CustomEvent("change"));
+		break;
+	case "BEFORE":
+		direction.selectedIndex = 2;
+		direction.dispatchEvent(new CustomEvent("change"));
+		break;
+	default:
+		//document.getElementById("closer").click();
+		return false;
+	}
+	document.getElementById("done").click();
 	return true;
 }
 
